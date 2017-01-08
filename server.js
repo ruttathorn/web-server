07b8d7f948584d8ffc56cbd2a2e495bba2512582
Ruttathorn Panassorn <ruttathorn@hotmail.com>
2017-01-08 20:50:26 +0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req,res,next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req,res, next){
+
+		console.log('Request: '+ new Date().toString()+ ' '+ req.method + ' '+ req.originalUrl);
+		next();
+	}
+};
+/*app.get('/', function(req,res){
+	res.send('Hello Express!');
+	
+});*/
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication,function(req,res){
+	res.send('Fintech ใหม่ ไฟแรง');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('Express server start on port '+ PORT);
+});
